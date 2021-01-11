be7a52daee5e806ecd49eade8c003f365bf981b9
Nada <olayan.nada@nagoya-u.jp>
2021-01-11 21:24:34 +0900

    connected to the websocket and validated jwt token for the uer correctly

--- src/websocket.ts
+++ src/websocket.ts
@@ -46,41 +46,28 @@ wss.on('connection', function connection(ws: CustomWebSocket) {
 				})
 			);
 		}
+		//server.listen(1338);
 		//ws.send(JSON.stringify({ ...message, user: 'self', intent: 'chat' })); //messges from self for now
 		console.log(message, 'is the message');
 	});
 });
 
 //reference from authentication : https://github.com/websockets/ws
-//// This function is not defined on purpose. Implement it with your own logic.
+server.on('upgrade', function upgrade(request, socket, head) {
+	const token = request.url.slice(1); //remove the backslash from the token url that we got from request
+	//console.log('REQUEST', request);
+	const payload: any = jwt.verify(token, process.env.JWT_SECRET_TOKEN); //verfity if token is correctly signed and change the type?
+	if (!payload) {
+		socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+		socket.destroy();
+		return;
+	}
+	wss.handleUpgrade(request, socket, head, function done(ws) {
+		const _ws = ws as CustomWebSocket;
+		_ws.connectionID = payload.email;
 
-// const http = require('http');
-// const WebSocket = require('ws');
-
-// const server = http.createServer();
-// const wss = new WebSocket.Server({ noServer: true });
-
-// wss.on('connection', function connection(ws, request, client) {
-//   ws.on('message', function message(msg) {
-//     console.log(`Received message ${msg} from user ${client}`);
-//   });
-// });
-
-// server.on('upgrade', function upgrade(request, socket, head) {
-//   // This function is not defined on purpose. Implement it with your own logic.
-//   authenticate(request, (err, client) => {
-//     if (err || !client) {
-//       socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
-//       socket.destroy();
-//       return;
-//     }
-
-//     wss.handleUpgrade(request, socket, head, function done(ws) {
-//       wss.emit('connection', ws, request, client);
-//     });
-//   });
-// });
-
-// server.listen(8080);
+		wss.emit('connection', ws, request);
+	});
+});
 
 server.listen(1338);
