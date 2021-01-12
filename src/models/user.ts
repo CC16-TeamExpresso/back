77bedbf0c66d5ec895d08f075fb635f133dc8096
Soshi <SoshiK@users.noreply.github.com>
2021-01-12 14:05:46 +0900

    added posts to user schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
+import Post from "./post";
 
 const UserModel = new mongoose.Schema(
 	{
@@ -6,7 +7,8 @@ const UserModel = new mongoose.Schema(
 		password: { type: String, required: true }, //I didnt use unique here because two people can have the same password
 		username: {type: String, required: true},
 		lat: {type: Number, required: false},
-		lng: {type: Number, required: false}
+		lng: {type: Number, required: false},
+		posts: [{type: Schema.Types.ObjectId, ref: Post, required: false}]
 	},
 	{ collection: 'users' }
 );
