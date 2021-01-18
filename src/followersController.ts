a305cf32795400af5f4d9c0f3808ed2ecbca309a
Soshi <SoshiK@users.noreply.github.com>
2021-01-18 17:36:17 +0900

    added getOwnFollower function

--- src/followersController.ts
+++ src/followersController.ts
@@ -1,4 +1,5 @@
 import Follower from "./models/followers";
+import User from "./models/user";
 
 export const addFollower = async (req, res) => {
   const userOwn = res.locals.user;
@@ -13,4 +14,35 @@ export const addFollower = async (req, res) => {
   } else {
     res.json({status: "ng"});
   }
+}
+
+interface followerObj{
+  id: String,
+  username: String,
+  post: any
+}
+
+export const getOwnFollower = async (req, res) => {
+  const userOwn = res.locals.user;
+  const followers = await  Follower.findOne({_id: userOwn.followers});
+  const result = [];
+  for(const follower of followers.followers) {
+    const user = await User.findOne({_id: follower})
+      .select("_id username posts")
+      .populate({
+        path:"posts",
+      })
+      .exec();
+    result.push(user);
+  }
+  const resultObj :followerObj[] = [];
+  result.forEach((data) => {
+    const obj : followerObj = {
+      id: data._id,
+      username: data.username,
+      post: data.posts[data.posts.length - 1]
+    }
+    resultObj.push(obj);
+    })
+  res.json({result: resultObj});
 }
