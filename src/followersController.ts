4e5453b155eba2d13afece90f477358e46a475d4
Soshi <SoshiK@users.noreply.github.com>
2021-01-20 12:06:16 +0900

    modified to return ordered array

--- src/followersController.ts
+++ src/followersController.ts
@@ -1,5 +1,6 @@
 import Follower from "./models/followers";
 import User from "./models/user";
+import { calcDistance } from "./userController";
 
 export const addFollower = async (req, res) => {
   const userOwn = res.locals.user;
@@ -17,9 +18,12 @@ export const addFollower = async (req, res) => {
 }
 
 interface followerObj{
-  id: String,
+  userid: String,
   username: String,
-  post: any
+  post: any,
+  lat: String,
+  lng: String,
+  distance: number
 }
 
 export const getOwnFollower = async (req, res) => {
@@ -28,7 +32,7 @@ export const getOwnFollower = async (req, res) => {
   const result = [];
   for(const follower of followers.followers) {
     const user = await User.findOne({_id: follower})
-      .select("_id username posts")
+      .select("_id username posts lat lng")
       .populate({
         path:"posts",
       })
@@ -38,11 +42,15 @@ export const getOwnFollower = async (req, res) => {
   const resultObj :followerObj[] = [];
   result.forEach((data) => {
     const obj : followerObj = {
-      id: data._id,
+      userid: data._id,
       username: data.username,
-      post: data.posts[data.posts.length - 1]
+      post: data.posts[data.posts.length - 1],
+      lat: data.lat,
+      lng: data.lng,
+      distance: calcDistance(userOwn.lat, userOwn.lng, data.lat, data.lng)
     }
     resultObj.push(obj);
     })
+  resultObj.sort((x, y) => x.distance - y.distance);
   res.json({result: resultObj});
 }
